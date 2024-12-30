ee727bfc7d6a5923720e3f474a1797624e45c76a
Abdulrahman <abdulrahmanhallak04@gmail.com>
2024-12-30 23:05:16 +0300

    remove access to courses from course/ url
    course is dependent resource of department so it should
    only be accessed through department

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path("department/", views.DepartmentListCreateView.as_view()),
-    # path("department/<int:pk>/course/", views.CourseListCreateView.as_view()),
     path("student/", include("api.student.urls")),
     path("department/", include("api.department.urls")),
-    path("course/", include("api.department.urls")),
     path("instructor/", include("api.instructor.urls")),
 ]
